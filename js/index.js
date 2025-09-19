f17d6668c2cb0e0b3ac0eae061f8e42f70a65c29
mohammademon393 <mdemon357ep@gmail.com>
2025-09-20 00:03:55 +0600

    load new data and showing the dispaly

--- js/index.js
+++ js/index.js
@@ -1,24 +1,72 @@
 // get the parent element
 const categoriesContainer = document.getElementById('navber');
+// news-container
+const newsContainer = document.getElementById('news-container');
 
-//categories data loaded
-const categoriesData =()=>{
-    const url = fetch('https://news-api-fs.vercel.app/api/categories');
-    url.then(res => res.json())
+// categories data loaded
+const categoriesData = () => {
+    fetch('https://news-api-fs.vercel.app/api/categories')
+        .then(res => res.json())
         .then(data => displayCategories(data))
+        .catch(err => console.error("Error loading categories:", err));
 }
 
-const displayCategories = (data)=>{
-    console.log(data.categories);
+
+const displayCategories = (data) => {
     const categories = data.categories;
-    categories.forEach(cetegory => {
-        categoriesContainer.innerHTML += `
-        <li class='hover:border-b-2 border-red-500 cursor-pointer'>${cetegory.title}<li/>
-        
-        `;
+
+    // clear old data
+    categoriesContainer.innerHTML = "";
+
+    categories.forEach(category => {
+        categoriesContainer.insertAdjacentHTML("beforeend", `
+            <li id="${category.id}" 
+                class="hover:border-b-4 border-red-500 cursor-pointer">
+                ${category.title}
+            </li>
+        `);
     });
+
+    // add click event only once (on parent - event delegation)
+    categoriesContainer.addEventListener('click', (e) => {
+        if (e.target.tagName === 'LI') {
+            const allLi = categoriesContainer.querySelectorAll('li');
+            allLi.forEach(li => li.classList.remove('border-b-4'));
+
+            e.target.classList.add('border-b-4');
+            // console.log("Selected category id:", e.target.id);
+            newsCategories(e.target.id);
+        }
+    });
+}
+
+
+// category data loaded/{id}
+const newsCategories = (newsId) => {
+
+    fetch(`https://news-api-fs.vercel.app/api/categories/${newsId}`)
+    .then(res => res.json())
+        .then(data => displayNews(data.articles))
+    .catch(err => {
+        console.log(err);
+        
+    })
+}
+
+const displayNews = (articles)=>{
+    console.log(articles);
+    newsContainer.innerHTML ="";
+    articles.forEach(article => {
+        newsContainer.innerHTML += `
+        <div>
+            <div>
+                <img src="${article.image.srcset[8].url}"/>
+            </div>
+            <h2>${article.title}</h2>
+        </div>
+        `;
+    })
     
 }
-
 
 categoriesData();
